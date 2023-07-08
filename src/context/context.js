8b289c6aeb339b887e9339fa623ff1daf2683eb3
vatari <majestic13@gmail.com>
2023-07-08 09:06:41 +0300

    setting context

--- src/context/context.js
+++ src/context/context.js
@@ -1,7 +1,13 @@
-import React, { useState, useEffect } from 'react';
-import mockUser from './mockData.js/mockUser';
-import mockRepos from './mockData.js/mockRepos';
-import mockFollowers from './mockData.js/mockFollowers';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import mockUser from "./mockData.js/mockUser";
+import mockRepos from "./mockData.js/mockRepos";
+import mockFollowers from "./mockData.js/mockFollowers";
+import axios from "axios";
 
-const rootUrl = 'https://api.github.com';
+const rootUrl = "https://api.github.com";
+
+const GithubContext = React.createContext();
+
+const GitHubProvider = ({ children }) => {
+  return <GithubContext.Provider>{children}</GithubContext.Provider>;
+};
