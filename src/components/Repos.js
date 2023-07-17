009084b34574373e0c5afca8de7574569351e67c
vatari <majestic13@gmail.com>
2023-07-17 08:38:54 +0300

    get data from mock data dynalically in Repos component

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -5,7 +5,12 @@ import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
 
-  let languages = repos.reduce((total, item) => {}, {});
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    total[language] = language;
+    return total;
+  }, {});
 
   const chartData = [
     {
